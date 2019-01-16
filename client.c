d84c761b597cd4c6cfbaf26a671b9af432831658
raunakchowdhury <rchowdhury5@stuy.edu>
2019-01-15 22:31:33 -0500

    removed deprecated fxn

--- client.c
+++ client.c
@@ -1,24 +1,5 @@
 #include "networking.h"
 #include "coupGame.h"
-int respond(int server_socket);
-
-int respond(int server_socket){
-  /*
-   * Lets the client know if they should be responding or not
-   */
-   char buffer[BUFFER_SIZE];
-   read(server_socket, buffer, sizeof(buffer));
-   //printf("\nRecieved: [%s]\n", buffer);
-
-   // send junk to facilitate next read
-   // write(server_socket, "ye", sizeof("ye"));
-   if (strcmp(buffer, "y") == 0){
-     return 1;
-   }
-   // server will respond with "n" if it does not want the client to respond
-   return 0;
-}
-
 
 int main(int argc, char **argv) {
 
